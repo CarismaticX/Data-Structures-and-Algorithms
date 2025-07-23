75671dbde84f7cabdfac4b20b38c1a61fd35a26e
Shekhar Singh Kushwah <kushwahshekhar.singh44@gmail.com>
2025-07-23 14:18:30 +0530

    solved more patterns, fibonnaci and 2 leetcode problems

---
+++ Day4/fibbonaci.cpp
@@ -0,0 +1,27 @@
+//Print fibbonaci series 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n ;
+    cin>>n;
+
+    int a = 0;
+    int b =1 ;
+    cout <<a<<" "<<b<<" ";
+
+    for(int i =0; i<n; i++){
+        int sum = a +b;
+        cout<<sum << " ";
+
+        a = b;
+        b = sum;
+    }
+
+    
+
+    return 0;
+}
