4dbdeac05fea9b26c2ff39c6ecc95e9997ab39dc
Shekhar Singh Kushwah <kushwahshekhar.singh44@gmail.com>
2025-08-09 23:41:43 +0530

    Day 21: Solve string compression & adjacent duplicates removal problems

---
+++ Day21/1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+
+        string temp;
+        int i = 0;
+
+        while(i<s.length()){
+            if(temp.empty() || temp.back() != s[i]){
+                temp.push_back(s[i]);
+                i++;
+            }else{
+                temp.pop_back();
+                i++;
+            }
+        }
+        return temp;
+        
+    }
+};
