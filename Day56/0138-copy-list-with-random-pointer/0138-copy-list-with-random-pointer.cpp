62bc64a8902a1f8b0f2af71b6d80b97ffc982ff9
Shekhar Singh Kushwah <kushwahshekhar.singh44@gmail.com>
2025-09-14 02:32:24 +0530

    Day 56: Clone a Linked List with Random Pointer

---
+++ Day56/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,66 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+    private:
+    void InsertCopyNode(Node*head){
+        Node*temp = head;
+
+        while(temp!=NULL){
+            Node*copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp->next->next;
+        }
+    }
+
+
+    void connectRandomPointer(Node*head){
+        Node*temp= head;
+
+        while(temp!=NULL){
+            Node*copyNode = temp->next;
+
+            if(temp->random){
+                copyNode->random = temp->random->next;
+
+            }
+            temp = temp->next->next;
+        }
+    }
+
+    Node*connectNextPointer(Node*head){
+
+        Node*dummyNode = new Node(-1);
+        Node*res = dummyNode;
+        Node*temp = head;
+        while(temp!=NULL){
+            res->next = temp->next;
+            temp->next = temp->next->next;
+
+            res = res->next;
+            temp = temp->next;
+        }
+        return dummyNode->next;
+    }
+public:
+    Node* copyRandomList(Node* head) {
+        InsertCopyNode(head);
+        connectRandomPointer(head);
+        return connectNextPointer(head);
+        
+    }
+};
