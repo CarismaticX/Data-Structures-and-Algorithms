e9e4e34fab9c3e90d0fef9108360d3886174047e
Shekhar Singh Kushwah <kushwahshekhar.singh44@gmail.com>
2025-10-12 00:01:36 +0530

    Day 84: Vertical Order Traversal of a Binary Tree

---
+++ Day84/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int, map<int, multiset<int>>> nodes;
+        queue<pair<TreeNode*, pair<int, int>>> q;
+
+        q.push({root, {0, 0}});
+
+        while (!q.empty()) {
+            auto [node, pos] = q.front();
+            q.pop();
+            int x = pos.first, y = pos.second;
+            nodes[x][y].insert(node->val);
+
+            if (node->left) q.push({node->left, {x - 1, y + 1}});
+            if (node->right) q.push({node->right, {x + 1, y + 1}});
+        }
+
+        vector<vector<int>> ans;
+        for (auto& [x, levelMap] : nodes) {
+            vector<int> col;
+            for (auto& [y, vals] : levelMap) {
+                col.insert(col.end(), vals.begin(), vals.end());
+            }
+            ans.push_back(col);
+        }
+        return ans;
+    }
+};
